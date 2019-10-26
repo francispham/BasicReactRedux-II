91697e21f4131516209ac5875fd67bd15bcfd5d9
Francis <francispham89@gmail.com>
2019-10-26 00:56:57 -0700

    Implementing Fetch Single User for All Posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,16 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+// Fetching All Posts:
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
+
+// Fetching A Single User:
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+};
+
